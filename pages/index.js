eb75d882a7005811472527fc1aaec7a8b420383e
AtharvNadkarni2013 <atharvmnadkarni@gmail.com>
2021-11-18 20:37:11 +0530

    Changed H1 tag to MUI Typography

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Typography from '@mui/material'
 
 export default function Home() {
   return (
-    <h1>Doomsday App</h1>
+    <Typography variant="h1">Doomsday App</Typography>
   )
 }
